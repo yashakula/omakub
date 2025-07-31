10cb29f27142e3f12dee0fbd122de0c8d393ebbd
Yash Akula <akulayash1996@gmail.com>
2025-07-30 20:56:46 -0700

    keep ubuntu dock

--- install/desktop/set-gnome-extensions.sh
+++ install/desktop/set-gnome-extensions.sh
@@ -6,7 +6,7 @@ pipx install gnome-extensions-cli --system-site-packages
 # Turn off default Ubuntu extensions
 gnome-extensions disable tiling-assistant@ubuntu.com
 gnome-extensions disable ubuntu-appindicators@ubuntu.com
-gnome-extensions disable ubuntu-dock@ubuntu.com
+# gnome-extensions disable ubuntu-dock@ubuntu.com
 gnome-extensions disable ding@rastersoft.com
 
 # Pause to assure user is ready to accept confirmations
